518b5ed1c4e8ac05d2f117059b43b599b3921a95
Ali Almohammad <ali@iotechsys.com>
2022-09-21 12:01:31 +0100

    Updated for helm install

--- master-bootstrap.sh
+++ master-bootstrap.sh
@@ -20,6 +20,4 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Install Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-${HOME}/get_helm.sh
+curl -s https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
